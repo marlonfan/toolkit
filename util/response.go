a7f30dfea8045742c1730dd3461d61decc1780bd
Marlon Fan <marlon@tfan.net>
2018-04-12 23:01:25 +0800

    add

---
+++ util/response.go
@@ -0,0 +1,39 @@
+package util
+
+import "encoding/json"
+
+// APIResult marlon's application api result
+type APIResult struct {
+	Code    int         `json:"code"`
+	Message interface{} `json:"msg"`
+	Data    interface{} `json:"data"`
+}
+
+// NewAPIResult 返回一个response对象
+func NewAPIResult() *APIResult {
+	return &APIResult{
+		Code:    0,
+		Message: ""}
+}
+
+// Success 成功对象
+func (a *APIResult) Success(data interface{}) *APIResult {
+	a.Data = data
+	return a
+}
+
+// Failed 失败对象
+func (a *APIResult) Failed(code int, message interface{}, data interface{}) *APIResult {
+	a.Code = code
+	a.Message = message
+	a.Data = data
+	return a
+}
+
+// ToJSON return a json byte
+func (a *APIResult) ToJSON() []byte {
+	if json, err := json.Marshal(a); err == nil {
+		return json
+	}
+	return []byte("")
+}
---
+++ util/validate.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"strings"
+)
+
+// some code for govalidator helper
+
+// ValidateErrorMessage 验证错误信息格式
+type ValidateErrorMessage map[string]interface{}
+
+// ParseValidateString 为验证错误结果格式化信息
+func ParseValidateString(err string) ValidateErrorMessage {
+	var errorMessage ValidateErrorMessage
+
+	arrArray := strings.Split(err, ";")
+	for _, i := range arrArray {
+		errItem := strings.Split(i, ": ")
+		errorMessage[errItem[0]] = errItem[1]
+	}
+	return errorMessage
+}
